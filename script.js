fdb0580805177b3593bc3fba049f6ff9bca16498
MatVzn <matteovoleite@gmail.com>
2024-03-02 22:58:45 -0300

    add tasks function work

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const html = document.documentElement
+function newTask() {
+  const taskArea = document.querySelector('.taskArea')
+  let div = document.createElement('div')
+  div.setAttribute('class', 'taskBox')
+  div.innerHTML = taskTemplate
 
-function addTask() {
-  alert('test')
+  taskArea.appendChild(div)
 }
+
+const checkBox = document.querySelectorAll('#checkBox')
+
+checkBox.forEach(checkBox => {
+  checkBox.addEventListener('click', () => {
+    if (checkBox.src.includes('square.svg')) {
+      checkBox.src = './assets/check.svg'
+    } else {
+      checkBox.src = './assets/square.svg'
+    }
+  })
+})
+
+
+
+const taskTemplate = 
+`
+  
+    <img src="./assets/square.svg" id="checkBox" onclick="checkTask()">
+    <input maxlength="37"></input>
+  
+`
